353bbd80fca936d351f7eed41b272a3cd6669a87
KassidyPrice <96833138+KassidyPrice@users.noreply.github.com>
2022-10-29 21:45:02 -0600

    fixed methods and error handling

--- index.js
+++ index.js
@@ -15,66 +15,91 @@ class ATMmachineVisit {
     this.balance = balance
   }
 
+  checkBalance() {
+    alert(`Your balance is ${this.balance}`)
+    this.greeting()
+  }
+
+  makeQuickWithdrawl() {
+    if (this.balance >= 20) {
+      const newBalance = this.balance - 20
+      this.balance = newBalance
+      alert(`Your new balance is ${this.balance}`)
+      this.greeting()
+    } else {
+      alert(`Insufficient Funds`)
+      this.greeting()
+    }
+  }
+
+  makeAmountWithdrawl() {
+    const withdrawlAmt = parseInt(
+      prompt(`How much would you like to withdraw?`)
+    )
+    if (this.balance >= withdrawlAmt) {
+      const newBalance = this.balance - withdrawlAmt
+      this.balance = newBalance
+      alert(`Your new balance is ${this.balance}`)
+      this.greeting()
+    } else {
+      alert(`Insufficient Funds`)
+      this.greeting()
+    }
+  }
+
+  makeWithdrawl() {
+    const withdrawlType = prompt(
+      `1) Quick Withdrawl ($20)\n2) Choose Withdrawl Amount`
+    )
+    if (withdrawlType === '1') {
+      this.makeQuickWithdrawl()
+    } else if (withdrawlType === '2') {
+      this.makeAmountWithdrawl()
+    } else {
+      alert(`Please make a valid choice.`)
+      this.greeting()
+    }
+  }
+
+  makeQuickDeposit() {
+    const newBalance = this.balance + 20
+    this.balance = newBalance
+    alert(`Your new balance is ${this.balance}`)
+    this.greeting()
+  }
+
+  makeAmountDeposit() {
+    const depositAmt = parseInt(prompt(`How much would you like to deposit?`))
+    const newBalance = this.balance + depositAmt
+    this.balance = newBalance
+    alert(`Your new balance is ${this.balance}`)
+    this.greeting()
+  }
+
+  makeDeposit() {
+    const depositType = prompt(
+      `1) Quick Deposit ($20)\n2) Choose Deposit Amount`
+    )
+    if (depositType === '1') {
+      this.makeQuickDeposit()
+    } else if (depositType === '2') {
+      this.makeAmountDeposit()
+    } else {
+      alert(`Please make a valid choice.`)
+      this.greeting()
+    }
+  }
+
   greeting() {
     const choice = prompt(
       `Welcome ${this.fname} ${this.lname} \nHow can I help you? \n1) view balance\n2) withdrawal\n3) deposit\n4) exit`
     )
-
     if (choice === '1') {
-      alert(`Your balance is ${this.balance}`)
-      this.greeting()
+      this.checkBalance()
     } else if (choice === '2') {
-      const withdrawlType = prompt(
-        `1) Quick Withdrawl ($20)\n2) Choose Withdrawl Amount`
-      )
-      if (withdrawlType === '1') {
-        if (this.balance >= 20) {
-          const newBalance = this.balance - 20
-          this.balance = newBalance
-          alert(`Your new balance is ${this.balance}`)
-          this.greeting()
-        } else {
-          alert(`Insufficient Funds`)
-          this.greeting()
-        }
-      } else if (withdrawlType === '2') {
-        const withdrawlAmt = parseInt(
-          prompt(`How much would you like to withdraw?`)
-        )
-        if (this.balance >= withdrawlAmt) {
-          const newBalance = this.balance - withdrawlAmt
-          this.balance = newBalance
-          alert(`Your new balance is ${this.balance}`)
-          this.greeting()
-        } else {
-          alert(`Insufficient Funds`)
-          this.greeting()
-        }
-      } else {
-        alert(`Please make a valid choice.`)
-        this.greeting()
-      }
+      this.makeWithdrawl()
     } else if (choice === '3') {
-      const depositType = prompt(
-        `1) Quick Deposit ($20)\n2) Choose Deposit Amount`
-      )
-      if (depositType === '1') {
-        const newBalance = this.balance + 20
-        this.balance = newBalance
-        alert(`Your new balance is ${this.balance}`)
-        this.greeting()
-      } else if (depositType === '2') {
-        const depositAmt = parseInt(
-          prompt(`How much would you like to deposit?`)
-        )
-        const newBalance = this.balance + depositAmt
-        this.balance = newBalance
-        alert(`Your new balance is ${this.balance}`)
-        this.greeting()
-      } else {
-        alert(`Please make a valid choice.`)
-        this.greeting()
-      }
+      this.makeDeposit()
     } else if (choice === '4') {
       alert(`Thank you! Have a nice day ${this.fname}`)
     } else {
@@ -84,6 +109,6 @@ class ATMmachineVisit {
   }
 }
 
-const Kassidy = new ATMmachineVisit('Kassidy', 'Price', 0)
+const Kassidy = new ATMmachineVisit('Kassidy', 'Price', 100)
 
 console.log(Kassidy.greeting())
